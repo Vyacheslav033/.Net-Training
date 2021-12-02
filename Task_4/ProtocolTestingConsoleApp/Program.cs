05c273b413d925ad84ad88870a405e4247214bfb
Вячеслав Украинцев <slavalapin033@gmail.con>
2021-12-02 14:12:36 +0300

    Создание проекта для 4-ого таска

---
+++ Task_4/ProtocolTestingConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtocolTestingConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
